d76b816bd80422550eae00585e484143ffde7cdc
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2021-01-08 21:48:37 +0800

    Add files via upload

---
+++ textDisplayScript/txts033.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class txts033 : MonoBehaviour
+{
+    public static string textDisplay = "";
+    Text txt;
+
+    void Start()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = textDisplay;
+    }
+}
---
+++ textDisplayScript/txts092.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class txts092 : MonoBehaviour
+{
+    public static string textDisplay = "";
+    Text txt;
+
+    void Start()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = textDisplay;
+    }
+}
