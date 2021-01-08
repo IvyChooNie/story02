0c19fae76beaf5c4fc564d3a771f95d2cdce2e84
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2021-01-08 21:33:28 +0800

    Add files via upload

---
+++ score/scoreA.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreA : MonoBehaviour
+{
+    public static int Score = 0;
+
+
+    private void OnGUI()
+    {
+        GUI.skin.box.fontSize = 34;
+        GUI.Box(new Rect(120, 100, 250, 120), "Score: " + Score.ToString());
+
+
+
+    }
+}
